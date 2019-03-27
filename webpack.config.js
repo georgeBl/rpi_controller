1c77ed08de5361b33e24b93318aef4c0e9036105
georgeBl <blanarugeorge96@gmail.com>
2019-03-27 17:05:34 +0000

    fixed heroku

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   entry: ['babel-polyfill', './src/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -32,6 +33,7 @@ module.exports = {
   devServer: {
     port: 3000,
     open: true,
+    historyApiFallback: true,
     proxy: {
       '/api': 'http://localhost:8080'
     }
@@ -42,5 +44,62 @@ module.exports = {
       template: './public/index.html',
       favicon: './public/favicon.ico'
     })
-  ]
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 };
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+//
+// const outputDirectory = 'dist';
+//
+// module.exports = {
+//   entry: ['babel-polyfill', './src/client/index.js'],
+//   output: {
+//     path: path.join(__dirname, outputDirectory),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader'
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+//         loader: 'url-loader?limit=100000'
+//       }
+//     ]
+//   },
+//   devServer: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': 'http://localhost:8080'
+//     }
+//   },
+//   plugins: [
+//     new CleanWebpackPlugin([outputDirectory]),
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html',
+//       favicon: './public/favicon.ico'
+//     })
+//   ],
+//   performance: {
+//     hints: false,
+//     maxEntrypointSize: 512000,
+//     maxAssetSize: 512000
+//   }
+// };
